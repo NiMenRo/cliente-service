04055f23e4de9f9969c84ef320793de44d170f4a
Nicolas Rojas <nicolas.mendoza@correounivalle.edu.co>
2025-06-12 17:26:46 -0500

    Verificacion del workflow de GitHub Actions

--- src/main/java/com/denkitronik/clienteservice/ClienteServiceApplication.java
+++ src/main/java/com/denkitronik/clienteservice/ClienteServiceApplication.java
@@ -3,6 +3,7 @@ package com.denkitronik.clienteservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Verificacion del workflow de GitHub Actions
 @SpringBootApplication
 public class ClienteServiceApplication {
 
